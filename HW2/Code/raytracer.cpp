9d16184a16fc1ea61d729b9829335a0189c9fa5e
manderfeld <mande174@umn.edu>
2018-10-09 21:40:55 -0500

    Working on a black image output that is the correct size

--- HW2/Code/raytracer.cpp
+++ HW2/Code/raytracer.cpp
@@ -1,5 +1,6 @@
 //File parsing example
 
+#include "image.h"
 #include <cstdio>
 #include <iostream>
 #include <fstream>
@@ -14,17 +15,19 @@
 
 using namespace std;
 
-int main(){
-  string line;
+int main(int argc, char* argv[]){
 
-  string fileName = "../Scenes/spheres1.scn";
+	const char* c;
+  string line;
+cout << argv[1] << endl;
+  //string fileName = "../Scenes/spheres1.scn";
 
   // open the file containing the scene description
-  ifstream input(fileName);
+  ifstream input(argv[1]);
 
   // check for errors in opening the file
   if(input.fail()){
-    cout << "Can't open file '" << fileName << "'" << endl;
+    cout << "Can't open file '" << argv[1] << "'" << endl;
     return 0;
   }
   
@@ -33,7 +36,7 @@ int main(){
   begin = input.tellg();
   input.seekg(0, ios::end);
   end = input.tellg();
-  cout << "File '" << fileName << "' is: " << (end-begin) << " bytes long.\n\n";
+  cout << "File '" << argv[1] << "' is: " << (end-begin) << " bytes long.\n\n";
   input.seekg(0, ios::beg);
 
   
@@ -60,14 +63,35 @@ int main(){
     }
     else if (command == "output_image"){ //If the command is an output_image command
        string outFile;
+       c = outFile.c_str();
        input >> outFile;
-       printf("Render to file named: %s\n", outFile.c_str());
+       printf("Render to file named: %s\n", c);
     }
     else {
       getline(input, line); //skip rest of line
       cout << "WARNING. Do not know command: " << command << endl;
     }
   }
+/*
+int n = argv[1].length();
+char* w = new char[n+1];
+copy(fileName.begin(), fileName.end(), w);
+w[n] = '\0';
+*/
+//char* c = fileName.c_str();
+/*char ar[n+1];
+strcpy(ar, fileName.c_str());
+for (int i=0; i<n; i++)
+{
+	cout << ar[i];
+}*/
+
+char* w = (char*)c;
+Image *img = NULL;
+img = new Image(w);
+
+
+//delete[] w;
   
   return 0;
 }
