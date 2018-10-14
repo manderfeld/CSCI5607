b5b476af374a080e94ec9705f0f0e9efa92dcf6c
manderfeld <mande174@umn.edu>
2018-10-13 20:55:25 -0500

    Vec3 addition, subtration

--- HW2/Code/raytracer.cpp
+++ HW2/Code/raytracer.cpp
@@ -21,23 +21,23 @@ struct Vec3
 	float x, y, z;
 	Vec3() : x(0.0), y(0.0), z(0.0) {}
 	Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
-	const float Magnitude() const // don't want to accidentally change values
+	const float Magnitude() const // don't want to accidentally change values here
 	{
 		return sqrt( x*x + y*y + z*z);
 	}
-	const Vec3 UnitVector() const
+	const Vec3 UnitVector() const // don't want to accidentally change values here
 	{
 		const float magnitude = Magnitude();
 		return Vec3(x/magnitude, y/magnitude, z/magnitude);
 	}
-	Vec3& operator += (const Vec3& a)
+	Vec3& operator += (const Vec3 &a) // don't want to change a's values
 	{
 		x += a.x;
 		y += a.y;
 		z += a.z;
 		return *this;
 	}
-	Vec3& operator *= (const Vec3& a)
+	Vec3& operator *= (const Vec3 &a) // don't want to change a's values
 	{
 		x *= a.x;
 		y *= a.y;
@@ -45,6 +45,15 @@ struct Vec3
 		return *this;
 	}
 };
+
+Vec3 operator + (const Vec3 &a, const Vec3 &b)
+{
+	return Vec3( a.x+b.x, a.y+b.y, a.z+b.z);
+}
+Vec3 operator - (const Vec3 &a, const Vec3 &b)
+{
+	return Vec3( a.x-b.x, a.y-b.y, a.z-b.z);
+}
 
 using namespace std;
 
