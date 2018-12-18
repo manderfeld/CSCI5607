f10df8ce8e6e1428b0a506a393025e7b258facd4
manderfeld <mande174@umn.edu>
2018-12-17 19:42:30 -0600

    Other minor changes

--- Final/Code/mbrot.c
+++ Final/Code/mbrot.c
@@ -10,10 +10,11 @@ void bn_handler(int bn, int state, int x, int y);
 void mouse_handler(int x, int y);
 
 unsigned int prog;
-float cx = 0.7, cy = 0.0;
-float scale = 2.2;
+double cx = 0.7, cy = 0.0;
+double scale = 2.2;
 int iter = 70;
-const float zoom_factor = 0.025;
+//const double zoom_factor = 0.001;
+const double zoom_factor = 0.008;
 
 int main(int argc, char **argv) {
 	void *img;
@@ -23,7 +24,7 @@ int main(int argc, char **argv) {
 	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutCreateWindow("Mindlapse :: GLSL Mandelbrot");
+	glutCreateWindow("Mandelbrot Set");
 
 	glutDisplayFunc(draw);
 	glutIdleFunc(idle_handler);
@@ -95,53 +96,65 @@ void key_handler(unsigned char key, int x, int y) {
 		printf("iterations: %d\n", iter);
 		set_uniform1i(prog, "iter", iter);
 		break;
-	case 'w':
-		scale *= 1 - zoom_factor * 2.0;
-		printf("scale: %f\n", scale);
+	case 'f':
+		scale *= (double)(1 - zoom_factor * 2.0);
+		printf("scale: %20.18f\n", scale);
 		break;
 	case 'd':
-		scale *= 1 + zoom_factor * 2.0;
+		scale *= (double)(1 + zoom_factor * 2.0);
 	default:
 		break;
 	}
 }
 
 int which_bn;
-float px, py;
+//int which_bn2;
+double px, py;
 
 void bn_handler(int bn, int state, int x, int y) {
 	
 	int xres = glutGet(GLUT_WINDOW_WIDTH);
 	int yres = glutGet(GLUT_WINDOW_HEIGHT);
-	px = 2.0 * ((float)x / (float)xres - 0.5);
-	py = 2.0 * ((float)y / (float)yres - 0.5);
-	which_bn = 1;
-	//which_bn = bn;
-/*
-	if(which_bn == 3) {
+	px = 2.0 * ((double)x / (double)xres - 0.5);
+	py = 2.0 * ((double)y / (double)yres - 0.5);
+	//which_bn = 1;
+	which_bn = bn;
+
+
+	if(which_bn == 3)
+	{
+		printf("UP\n");
 		scale *= 1 - zoom_factor * 2.0;
-	} else if(which_bn == 4) {
+	}
+	else if(which_bn == 4)
+	{
+		printf("DOWN\n");
 		scale *= 1 + zoom_factor * 2.0;;
-	}*/
+	}
+	else if(which_bn == 1)
+	{
+		//which_bn = 1;
+	}
 	
 }
 
 void mouse_handler(int x, int y) {
 	int xres = glutGet(GLUT_WINDOW_WIDTH);
 	int yres = glutGet(GLUT_WINDOW_HEIGHT);
-	float fx = 2.0 * ((float)x / (float)xres - 0.5);
-	float fy = 2.0 * ((float)y / (float)yres - 0.5);
+	double fx = 2.0 * ((double)x / (double)xres - 0.5);
+	double fy = 2.0 * ((double)y / (double)yres - 0.5);
 
-	//cx += (fx - px) * scale / 2.0;
-	//cy -= (fy - py) * scale / 2.0;
+	cx += (fx - px) * scale / 2.0;
+	cy -= (fy - py) * scale / 2.0;
 
-
+/*
 	if(which_bn == 1) {
 		cx += (fx - px) * scale / 2.0;
 		cy -= (fy - py) * scale / 2.0;
 	} else if(which_bn == 0) {
 		//scale *= (fy - py < 0.0) ? 1 - zoom_factor : 1 + zoom_factor;
 	}
+*/
 
 
 	px = fx;
